6fbcfae829c47d39ad7dc4ca351fe0487bfc672d
somkumarav <somkumarav@gmail.com>
2024-10-19 16:19:48 +0530

    Impr: album url by encrypting and preventing invalid url to upload

--- src/actions/album.actions.ts
+++ src/actions/album.actions.ts
@@ -1,6 +1,12 @@
 "use server";
 import { redirect } from "next/navigation";
 import prisma from "../../lib/prisma";
+import { decrypt, encrypt } from "../../lib/encryption";
+import { withServerActionAsyncCatcher } from "../../lib/async-catch";
+import { ServerActionReturnType } from "../../types/api.types";
+import { z } from "zod";
+import { SuccessResponse } from "../../lib/success";
+import { ErrorHandler } from "../../lib/error";
 
 export const createAlbum = async () => {
   const data = await prisma.album.create({
@@ -9,9 +15,35 @@ export const createAlbum = async () => {
     },
   });
 
+  const encryptedData = encrypt(`${data.id}`);
+  console.log("enc", encryptedData);
+
   if (data.id) {
-    redirect(`/album/${data.id}`);
+    redirect(`/album/${encryptedData}`);
   } else {
     throw new Error("Failed to create album");
   }
 };
+
+const checkIfAlbumExistsSchema = z.string();
+export const checkIfAlbumExists = withServerActionAsyncCatcher<
+  string,
+  ServerActionReturnType<unknown>
+>(async (args) => {
+  const validatedData = await checkIfAlbumExistsSchema.safeParse(args);
+  if (!validatedData.success) {
+    throw new ErrorHandler("Data validation failed", "BAD_REQUEST");
+  }
+  const decryptedData = decrypt(validatedData.data);
+  console.log("dec", decryptedData);
+
+  const data = await prisma.album.findFirst({
+    where: {
+      id: Number(decryptedData),
+    },
+  });
+  if (!data) {
+    throw new ErrorHandler("Invalid album", "BAD_REQUEST");
+  }
+  return new SuccessResponse("Album exists", 201).serialize();
+});
--- src/app/album/[id]/page.tsx
+++ src/app/album/[id]/page.tsx
@@ -1,7 +1,29 @@
 import { MultiImageUploader } from "@/components/multiple-image-uploader";
 import { ImageGallery } from "@/components/image-gallery";
+import { checkIfAlbumExists } from "@/actions/album.actions";
+import { Button } from "@/components/ui/button";
 
-const Page = ({ params }: { params: { id: string } }) => {
+const Page = async ({ params }: { params: { id: string } }) => {
+  const isValidAlbum = await checkIfAlbumExists(params.id);
+
+  if (!isValidAlbum.status) {
+    return (
+      <div className='pt-32 flex flex-col items-center justify-center space-y-4'>
+        <p className='text-5xl text-center font-semibold'>
+          Oops!
+          <br />
+          broken URL
+        </p>
+        <p className=''>
+          The URL seems to be broken, please check the URL and try again
+        </p>
+        <div>
+          <Button>Go Home</Button>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div>
       <div className='flex w-full flex-col'>
