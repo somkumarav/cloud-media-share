d36523e01c4f8a439e88492d79976da3438134aa
somkumarav <somkumarav@gmail.com>
2024-10-19 12:47:18 +0530

    Install: shadcn and prisma installed button component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from "tailwindcss";
+import * as tailwindAnimate from "tailwindcss-animate";
 
 const config: Config = {
+  darkMode: ["class"],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,11 +10,63 @@ const config: Config = {
   ],
   theme: {
     colors: {
-      background: "#000212",
-      foreground: "#ffffff",
       white: "#ffffff",
+      black: "#000000",
+      background: {
+        DEFAULT: "#08090a",
+        secondary: "#28282c",
+      },
+      foreground: {
+        DEFAULT: "#e6e6e6",
+      },
+      accent: {
+        DEFAULT: "#173f35",
+        foreground: "#68cc55",
+        background: "#172217",
+      },
+      muted: {
+        DEFAULT: "#91170c",
+        foreground: "#eaa09c",
+      },
+      destructive: {
+        DEFAULT: "hsl(var(--destructive))",
+        foreground: "hsl(var(--destructive-foreground))",
+      },
+      card: {
+        DEFAULT: "hsl(var(--card))",
+        foreground: "hsl(var(--card-foreground))",
+      },
+      popover: {
+        DEFAULT: "hsl(var(--popover))",
+        foreground: "hsl(var(--popover-foreground))",
+      },
+
+      border: "hsl(var(--border))",
+      input: "hsl(var(--input))",
+      ring: "hsl(var(--ring))",
+      chart: {
+        "1": "hsl(var(--chart-1))",
+        "2": "hsl(var(--chart-2))",
+        "3": "hsl(var(--chart-3))",
+        "4": "hsl(var(--chart-4))",
+        "5": "hsl(var(--chart-5))",
+      },
+    },
+    space: {
+      "1": ".25rem",
+      "2": ".5rem",
+      "3": "1rem",
+      "4": "1.25rem",
+      "5": "1.5rem",
+    },
+    borderRadius: {
+      sm: "0.25rem",
+      md: "0.5rem",
+      lg: "1rem",
+      xl: "1.25rem",
+      full: "1000rem",
     },
   },
-  plugins: [],
+  plugins: [tailwindAnimate],
 };
 export default config;
