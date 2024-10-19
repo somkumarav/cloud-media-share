4d50e34f9aa47d03cd39e81291da30ee144cfa62
somkumarav <somkumarav@gmail.com>
2024-10-19 14:39:43 +0530

    Impr: nav-bar logo add redirect to home

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -1,5 +1,6 @@
 import { Logo } from "@/components/icons/logo";
 import { Button } from "./ui/button";
+import Link from "next/link";
 
 export const NavBar = () => {
   return (
@@ -7,7 +8,9 @@ export const NavBar = () => {
       // className='flex items-center justify-between border rounded-lg py-4 px-4 max-w-[1000px]'
       className='sticky top-0 z-50 w-full bg-background backdrop-blur supports-[backdrop-filter]:bg-background/60 border rounded-xl flex items-center justify-between max-w-[1000px] py-3 px-6 border-background-secondary mt-10'
     >
-      <Logo />
+      <Link href='/'>
+        <Logo />
+      </Link>
       <div>
         <div className='flex space-x-2'>
           <Button variant='secondary'>Log in</Button>
