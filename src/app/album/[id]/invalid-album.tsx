62f5cece9640a4a681d9da90c27faa5141503d14
somkumarav <somkumarav@gmail.com>
2024-10-20 13:24:26 +0530

    refactor: separated invalidAlbum to another component

---
+++ src/app/album/[id]/invalid-album.tsx
@@ -0,0 +1,36 @@
+import { CreateNewAlbumButton } from "@/components/createNewAlbum.Button";
+import { EncryptedText } from "@/components/encrypted-text";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
+export const InvalidAlbum = () => {
+  return (
+    <div className='pt-32 flex flex-col items-center justify-center'>
+      <EncryptedText
+        text='broken url encryption'
+        className='text-5xl text-center font-semibold'
+        leadingText={
+          <p className='text-5xl text-center font-semibold'>
+            Oops!
+            <br />
+          </p>
+        }
+        trailingText={
+          <>
+            <p className='mt-2 mb-8 text-muted'>
+              The URL seems to be broken, please check the URL and try again
+            </p>
+            <div className='flex items-center space-x-2'>
+              <Link href='/'>
+                <Button size='lg' variant='secondary'>
+                  Return to home
+                </Button>
+              </Link>
+              <CreateNewAlbumButton />
+            </div>
+          </>
+        }
+      />
+    </div>
+  );
+};
