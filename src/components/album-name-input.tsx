e51ac98aa8a59c8593e35447d8a7c6fe64822277
somkumarav <somkumarav@gmail.com>
2024-10-22 21:27:20 +0530

    Style: change album name submit button

--- src/components/album-name-input.tsx
+++ src/components/album-name-input.tsx
@@ -4,11 +4,15 @@ import { useForm } from "react-hook-form";
 import { AlbumNameSchema, TAlbumNameSchema } from "../../types/album";
 import { Button } from "./ui/button";
 import { changeAlbumName } from "../actions/album.actions";
+import { cn } from "../../lib/utils";
+import { Check, ChevronLeft } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 export const AlbumNameInput = (props: {
   albumId: string;
   albumName: string;
 }) => {
+  const router = useRouter();
   const form = useForm<TAlbumNameSchema>({
     resolver: zodResolver(AlbumNameSchema),
     defaultValues: {
@@ -18,7 +22,7 @@ export const AlbumNameInput = (props: {
 
   return (
     <form
-      className='flex items-center space-x-2 my-5'
+      className='w-full flex items-center space-x-2 my-5'
       onSubmit={form.handleSubmit(async (data) => {
         await changeAlbumName({
           albumId: props.albumId,
@@ -26,14 +30,26 @@ export const AlbumNameInput = (props: {
         });
       })}
     >
+      {form.watch("name") !== props.albumName ? (
+        <Button type='submit' size='icon'>
+          <Check strokeWidth={3} />
+        </Button>
+      ) : (
+        <ChevronLeft
+          className='cursor-pointer h-10 w-10'
+          onClick={() => {
+            router.back();
+          }}
+        />
+      )}
       <input
         type='text'
         {...form.register("name")}
-        className='bg-background text-white outline-none text-4xl'
+        className={cn(
+          "bg-background text-white outline-none text-4xl",
+          `w-[${form.watch("name").length * 5}px]`
+        )}
       />
-      {form.watch("name") !== props.albumName ? (
-        <Button type='submit'>submit</Button>
-      ) : null}
     </form>
   );
 };
