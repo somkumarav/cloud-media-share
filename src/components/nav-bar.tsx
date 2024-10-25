c8c0b654e82e5f80ec04dc70d95f8703727fd50d
Somkumar <somkumarav@gmail.com>
2024-10-25 22:31:50 +0530

    Style: reduce top margin for nav-bar

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/src/components/ui/button";
 export const NavBar = () => {
   return (
     <div className='sticky top-0 z-50 w-full flex items-center justify-center'>
-      <div className='bg-background backdrop-blur supports-[backdrop-filter]:bg-background/60 border rounded-xl flex items-center justify-between min-w-[1000px] max-w-[1400px] py-2 px-5 border-background-secondary mt-4'>
+      <div className='bg-background backdrop-blur supports-[backdrop-filter]:bg-background/60 border rounded-xl flex items-center justify-between min-w-[1000px] max-w-[1400px] py-2 px-5 border-background-secondary mt-2'>
         <Link href='/'>
           <Logo />
         </Link>
