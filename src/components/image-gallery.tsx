d0ec9fe8e763f85b0e8ec74cbb118907c4332fd1
somkumarav <somkumarav@gmail.com>
2024-11-11 19:56:44 +0530

    impr: loading time of thumbnail images

--- src/components/image-gallery.tsx
+++ src/components/image-gallery.tsx
@@ -12,24 +12,27 @@ export const ImageGallery = async ({ directory }: { directory: string }) => {
 
   return (
     <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4'>
-      {imageUrls.map((image) => (
-        <div key={image.key} className='relative aspect-square group'>
-          <Image
-            src={image.url}
-            alt={image.key}
-            className='object-cover w-full h-full rounded-lg'
-            unoptimized
-            height={100}
-            width={100}
-          />
-          <div className='absolute inset-0 bg-black bg-opacity-0 group-hover:bg-opacity-50 transition-opacity duration-200 flex items-center justify-center'>
-            <DownloadImageButton image={image} />
+      {imageUrls.map((image) =>
+        image.key.split("/")[1].startsWith("thumbnail") ? (
+          <div key={image.key} className='relative aspect-square group'>
+            <Image
+              loading='eager'
+              src={image.thumbnailUrl}
+              alt={image.key}
+              className='object-cover w-full h-full rounded-lg'
+              unoptimized
+              height={100}
+              width={100}
+            />
+            <div className='absolute inset-0 bg-black bg-opacity-0 group-hover:bg-opacity-50 transition-opacity duration-200 flex items-center justify-center'>
+              <DownloadImageButton image={image} />
+              <div className='hidden group-hover:block absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text- p-2 text-sm truncate'>
+                {image.key.split("/").pop()}
+              </div>
+            </div>
           </div>
-          <div className='absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text- p-2 text-sm truncate'>
-            {image.key.split("/").pop()}
-          </div>
-        </div>
-      ))}
+        ) : null
+      )}
     </div>
   );
 };
