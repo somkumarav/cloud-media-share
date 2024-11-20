2cb363e13ae0d6c5966ec59794988e3bf75bb237
somkumarav <somkumarav@gmail.com>
2024-11-20 22:01:09 +0530

    add: link to portfolio for the hero pillCTA

--- src/components/pillCTA.tsx
+++ src/components/pillCTA.tsx
@@ -1,9 +1,15 @@
+import Link from "next/link";
+
 export const PillCTA = () => {
   return (
     <div className='mt-32 mb-3 opacity-0 animate-top-fade-in [animation-delay:200ms;]'>
-      <div className='py-[2px] px-5 rounded-full bg-accent-background text-accent-foreground'>
-        Created with love by somu. 😶‍🌫️
-      </div>
+      <Link
+        href='https://portfolio-4rc.pages.dev'
+        target='_blank'
+        className='py-[2px] px-5 rounded-full bg-accent-background text-accent-foreground hover:bg-accent-foreground/20 text-xs md:text-base transition-colors duration-300'
+      >
+        Created with love by somu. 😶‍🌫
+      </Link>
     </div>
   );
 };
