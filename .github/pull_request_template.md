2f7b12fbb2b70b10dc77b1fb3ecd09c7606897d4
somkumarav <somkumarav@gmail.com>
2025-08-09 16:39:15 +0530

    converted to turborepo

--- .github/pull_request_template.md
+++
@@ -1,3 +0,0 @@
-- Closes #issue_number
-
-**Description**
--- types/api.types.ts
+++
@@ -1,6 +0,0 @@
-import { ErrorResponseType } from "@/lib/error";
-import { SuccessResponseType } from "@/lib/success";
-
-export type ServerActionReturnType<T = unknown> =
-  | SuccessResponseType<T>
-  | ErrorResponseType;
