52f2b4cff3d6ab12c2593f6786b9f0e26cc311c2
somkumarav <somkumarav@gmail.com>
2025-08-11 21:15:26 +0530

    add: more image details in the listImagesInDirectory action

--- apps/web/actions/r2.actions.ts
+++ apps/web/actions/r2.actions.ts
@@ -1,26 +1,25 @@
 "use server";
-import { GetObjectCommand, ListObjectsV2Command } from "@aws-sdk/client-s3";
+import { GetObjectCommand } from "@aws-sdk/client-s3";
 import { s3 } from "@/lib/s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import prisma from "@repo/db/client";
+import { decrypt } from "@/lib/encryption";
 
 export async function listImagesInDirectory(directory: string) {
-  const command = new ListObjectsV2Command({
-    Bucket: "test",
-    Prefix: `${directory}/thumbnail-`,
+  const decryptedAlbumId = Number(decrypt(directory));
+  const albumContent = await prisma.albumContents.findMany({
+    where: {
+      albumId: decryptedAlbumId,
+    },
   });
 
   try {
-    const response = await s3.send(command);
-    const objects = response.Contents || [];
-
-    const imageObjects = objects.filter((obj) =>
-      obj.Key?.toLowerCase().match(/\.(jpg|jpeg|png|gif|webp)$/)
-    );
+    const imageObjects = albumContent.filter((obj) => obj.fileType === "image");
 
     const imageUrls = await Promise.all(
       imageObjects.map(async (obj) => {
-        const imageName = obj.Key?.replace("thumbnail-", "");
-        const thumbnailImageName = obj.Key;
+        const imageName = `${directory}/${obj.fileName}`;
+        const thumbnailImageName = `${directory}/thumbnail-${obj.fileName}`;
 
         const getObjectCommand = new GetObjectCommand({
           Bucket: "test",
@@ -31,13 +30,13 @@ export async function listImagesInDirectory(directory: string) {
           Key: thumbnailImageName,
         });
 
-        const url = await getSignedUrl(s3, getObjectCommand, {
+        const imageUrl = await getSignedUrl(s3, getObjectCommand, {
           expiresIn: 36000,
         });
         const thumbnailUrl = await getSignedUrl(s3, getThumbnailObjectCommand, {
           expiresIn: 36000,
         });
-        return { key: obj.Key!, url, thumbnailUrl };
+        return { ...obj, imageUrl, thumbnailUrl };
       })
     );
 
