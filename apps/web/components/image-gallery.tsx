0f8d62fc8d95ea29b574ffa40b002cba400913f1
somkumarav <somkumarav@gmail.com>
2025-08-11 21:17:47 +0530

    add: new card style for the image

--- apps/web/components/image-gallery.tsx
+++ apps/web/components/image-gallery.tsx
@@ -1,7 +1,6 @@
 import { listImagesInDirectory } from "@/actions/r2.actions";
-import Image from "next/image";
-import { DownloadImageButton } from "@/components/download-image-button";
-import { EmptyGallery } from "./empty-gallery";
+import { EmptyGallery } from "@/components/empty-gallery";
+import { IndividualImage } from "./individual-image";
 
 export const ImageGallery = async ({ directory }: { directory: string }) => {
   const imageUrls = await listImagesInDirectory(directory);
@@ -12,27 +11,9 @@ export const ImageGallery = async ({ directory }: { directory: string }) => {
 
   return (
     <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4'>
-      {imageUrls.map((image) =>
-        image.key.split("/")[1]?.startsWith("thumbnail") ? (
-          <div key={image.key} className='relative aspect-square group'>
-            <Image
-              loading='lazy'
-              src={image.thumbnailUrl}
-              alt={image.key}
-              className='object-cover w-full h-full rounded-lg'
-              unoptimized
-              height={100}
-              width={100}
-            />
-            <div className='absolute inset-0 bg-black bg-opacity-0 group-hover:bg-opacity-30 transition-opacity duration-200 flex items-center justify-center'>
-              <DownloadImageButton image={image} />
-              <div className='hidden group-hover:block absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text- p-2 text-sm truncate'>
-                {image.key.split("/").pop()}
-              </div>
-            </div>
-          </div>
-        ) : null
-      )}
+      {imageUrls.map((image) => (
+        <IndividualImage key={image.id} {...image} />
+      ))}
     </div>
   );
 };
---
+++ apps/web/components/individual-image.tsx
@@ -0,0 +1,86 @@
+import Image from "next/image";
+import { cva, VariantProps } from "class-variance-authority";
+import { cn, formatFileSize } from "@/lib/utils";
+import { DownloadImageButton } from "@/components/download-image-button";
+
+type TProps = {
+  imageUrl: string;
+  thumbnailUrl: string;
+  fileSize: number;
+  id: number;
+  fileName: string;
+  fileType: string;
+  url: string;
+  createdAt: Date;
+  albumId: number | null;
+};
+const IndividualImage = (image: TProps) => {
+  const fileSize = formatFileSize(image.fileSize);
+  return (
+    <div key={image.id} className='rounded-md border border-white/10'>
+      <div className='relative aspect-square group'>
+        <div className='absolute inset-0 bg-black bg-opacity-0 group-hover:bg-opacity-30 transition-opacity duration-200 flex items-center justify-center'>
+          <DownloadImageButton
+            image={{ key: image.fileName, url: image.imageUrl }}
+          />
+        </div>
+        <Image
+          loading='lazy'
+          src={image.thumbnailUrl}
+          alt={image.fileName}
+          className='object-cover w-full h-full rounded-t-md'
+          unoptimized
+          height={100}
+          width={100}
+        />
+      </div>
+      <div className='px-2 pt-3 pb-2'>
+        <p title={image.fileName} className='truncate'>
+          {image.fileName}
+        </p>
+        <div>
+          <FileSizePill
+            fileSize={fileSize.formatted}
+            className='-translate-x-[2px]'
+            variant={
+              image.fileSize > 1024 * 1000 * 5
+                ? image.fileSize > 1024 * 1000 * 5 &&
+                  image.fileSize <= 1024 * 1000 * 10
+                  ? "medium"
+                  : "large"
+                : "small"
+            }
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const pillVariants = cva("w-fit px-2 rounded-full mt-2 [&>p]:text-sm", {
+  variants: {
+    variant: {
+      small: "bg-accent-background [&>p]:text-accent-foreground",
+      medium: "bg-caution-background [&>p]:text-caution",
+      large: "bg-destructive-background [&>p]:text-destructive",
+    },
+  },
+  defaultVariants: {
+    variant: "small",
+  },
+});
+
+export interface PillProps extends VariantProps<typeof pillVariants> {
+  fileSize: string;
+  className?: string;
+}
+
+const FileSizePill = ({ fileSize, className, variant }: PillProps) => {
+  return (
+    <div className={cn(pillVariants({ variant, className }))}>
+      <p>{fileSize}</p>
+    </div>
+  );
+};
+
+export { IndividualImage, FileSizePill };
