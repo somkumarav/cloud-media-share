259772d7a20abe9f8334f700423a935d14e0f236
somkumarav <somkumarav@gmail.com>
2025-08-13 21:02:46 +0530

    other: some utility fix

--- reset.sh
+++ reset.sh
@@ -4,10 +4,15 @@ rm -rf node_modules \
   .turbo \
   apps/web/node_modules \
   apps/web/.next \
+  apps/worker/node_modules \
+  apps/worker/.turbo \
+  apps/worker/dist\
   packages/db/node_modules \
   packages/eslint-config/node_modules \
   packages/typescript-config/node_modules \
-  packages/ui/node_modules 
+  packages/ui/node_modules \
+  packages/queue/node_modules \
+  packages/queue/dist \
   
 
-echo "Cleaned up node_modules and typescript-config directories."
+echo "Deleted node_modules, .turbo, and build directories."
