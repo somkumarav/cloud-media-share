87e52c1a97d6a977dec8cf3cda10c558f3213072
somu <somkumarav@gmail.com>
2025-08-18 15:17:07 +0530

    add: download full album as zip functionality (#18)

--- apps/web/actions/download.actions.ts
+++ apps/web/actions/download.actions.ts
@@ -1,10 +1,10 @@
-// app/actions/download.ts (Server Action)
 "use server";
 
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { GetObjectCommand } from "@aws-sdk/client-s3";
 import { s3 } from "@/lib/s3";
 import prisma from "@repo/db/client";
+import { getDecryptedId } from "@repo/utils/index";
 
 export async function downloadImage(imageId: number) {
   const image = await prisma.media.findUnique({
@@ -28,4 +28,43 @@ export async function downloadImage(imageId: number) {
   });
 
   return signedUrl;
+}
+
+export async function downloadAlbumAsZip(encryptedToken: string) {
+  const albumId = getDecryptedId(encryptedToken);
+
+  const album = await prisma.album.findUnique({
+    where: { id: albumId },
+    include: {
+      media: true,
+    },
+  });
+
+  if (!album) {
+    throw new Error("Album not found");
+  }
+
+  const downloadURLs = await Promise.all(
+    album.media.map(async (image) => {
+      const command = new GetObjectCommand({
+        Bucket: "test",
+        Key: image.storageBucketKey,
+      });
+
+      const signedURL = await getSignedUrl(s3, command, {
+        expiresIn: 300,
+      });
+
+      return {
+        imageURL: signedURL,
+        filename: image.originalName,
+      };
+    })
+  );
+
+  return {
+    albumName: album.title,
+    images: downloadURLs,
+    totalSize: album.totalSize,
+  };
 }
